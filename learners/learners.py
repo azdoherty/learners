a06ff20a041f7d0690ee842c157d197748821e10
adoherty8 <F7U124life!>
2017-05-29 16:26:46 -0400

    add comments

--- learners/learners.py
+++ learners/learners.py
@@ -3,6 +3,15 @@ import numpy as np
 
 class Qlearner():
     def __init__(self, n_states, n_actions, alpha, gamma, random_rate=.1, random_decay=.999):
+        """
+        Q learner to
+        :param n_states: int - number of states in state space
+        :param n_actions: int - number of actions available at each state
+        :param alpha: float - learning rate
+        :param gamma: float - discount factor
+        :param random_rate: float - probability of making random action
+        :param random_decay: float - amount to decay random rate at end of each episode
+        """
         self.qtable = np.random.random((n_states, n_actions))
         self.n_states = n_states
         self.n_actions = n_actions
@@ -12,13 +21,25 @@ class Qlearner():
         self.random_decay = random_decay
 
     def predict(self, state):
-        explore = np.ranmdom.random()
+        """
+        :param state: int - current state of system
+        :return: optimal action with probability 1-random rate
+        """
+        explore = np.random.random()
         if explore < self.random_rate:
-            return np.random.randint((0, self.n_actions))
+            return np.random.randint(0, self.n_actions)
         else:
             return np.argmax(self.qtable[state, :])
 
     def update(self, state, action, reward, newstate, done):
+        """
+        :param state: int - previous state of system
+        :param action: int - action taken
+        :param reward: float - reward received
+        :param newstate: int - new state of system
+        :param done: boolean - is learning episode finished
+        :return:
+        """
         optimal = np.max(self.qtable[newstate, :])
         if not done:
             #self.qtable[state, action] += self.alpha * ( reward + self.gamma * (optimal - self.qtable[state, action]))
