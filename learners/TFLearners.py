40e6ed600ac76f754e095b889fa8177b369653c8
Adam Doherty <azdoherty91@gmail.com>
2018-08-11 18:08:10 -0400

    stuff TFlearner and class and build for external data sets

--- learners/TFLearners.py
+++ learners/TFLearners.py
@@ -1,26 +1,9 @@
 import tensorflow as tf
+import numpy as np
 # taken from example here: http://adventuresinmachinelearning.com/convolutional-neural-networks-tutorial-tensorflow/
 # added docstrings for clarity and worked comments directly into code
 # datasets to playwith
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-# optimization
-learning_rate = 0.0001
-epochs = 5
-batch_size = 500
-
-# declare the training data placeholders
-# input x - for 28 x 28 pixels = 784 - this is the flattened image data that is drawn from
-xpix = 28
-ypix = 28
-size = xpix * ypix
-x = tf.placeholder(tf.float32, [None, size])
-# dynamically reshape the input into 4d object as required by TF
-x_shaped = tf.reshape(x, [-1, 28, 28, 1])
-# now declare the output data placeholder - 10 digits possible
-y = tf.placeholder(tf.float32, [None, 10])
 
 def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_shape, pool_shape, name):
     """
@@ -59,59 +42,77 @@ def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_sh
     return out_layer
 
 # output of layer 1 must equal input channels of layer 2
-layer1 = create_new_conv_layer(x_shaped, 1, 32, [5, 5], [2, 2], name='layer1')
-layer2 = create_new_conv_layer(layer1, 32, 64, [5, 5], [2, 2], name='layer2')
-flattened = tf.reshape(layer2, [-1, 7 * 7 * 64])
-wd1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1000], stddev=0.03), name='wd1')
-bd1 = tf.Variable(tf.truncated_normal([1000], stddev=0.01), name='bd1')
-dense_layer1 = tf.matmul(flattened, wd1) + bd1
-dense_layer1 = tf.nn.relu(dense_layer1)
 
-# connect to the output layer
-wd2 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.03), name='wd2')
-bd2 = tf.Variable(tf.truncated_normal([10], stddev=0.01), name='bd2')
-dense_layer2 = tf.matmul(dense_layer1, wd2) + bd2
-y_ = tf.nn.softmax(dense_layer2)
+class TensorFlowNeuralNetwork:
+    def __init__(self, xshape, yshape, learning_rate=.0001):
+        # optimization
+        self.learning_rate = 0.0001
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=dense_layer2, labels=y))
+        # declare the training data placeholders
+        size = xshape * yshape
+        self.x = tf.placeholder(tf.float32, [None, size])
+        # dynamically reshape the input into 4d object as required by TF
+        x_shaped = tf.reshape(self.x, [-1, xshape, yshape, 1])
+        # now declare the output data placeholder - 10 digits possible
+        self.y = tf.placeholder(tf.float32, [None, 10])
 
-# add an optimiser
-optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+        layer1 = create_new_conv_layer(x_shaped, 1, 32, [5, 5], [2, 2], name='layer1')
+        layer2 = create_new_conv_layer(layer1, 32, 64, [5, 5], [2, 2], name='layer2')
+        flattened = tf.reshape(layer2, [-1, 7 * 7 * 64])
+        wd1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1000], stddev=0.03), name='wd1')
+        bd1 = tf.Variable(tf.truncated_normal([1000], stddev=0.01), name='bd1')
+        dense_layer1 = tf.matmul(flattened, wd1) + bd1
+        dense_layer1 = tf.nn.relu(dense_layer1)
+        wd2 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.03), name='wd2')
+        bd2 = tf.Variable(tf.truncated_normal([10], stddev=0.01), name='bd2')
+        dense_layer2 = tf.matmul(dense_layer1, wd2) + bd2
+        y_ = tf.nn.softmax(dense_layer2)
+        self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=dense_layer2, labels=self.y))
 
-# define an accuracy assessment operation
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # add an optimiser
+        self.optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cross_entropy)
 
-# setup the initialisation operator
-init_op = tf.global_variables_initializer()
+        # define an accuracy assessment operation
+        correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(y_, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# add an optimiser
+        # setup the initialisation operator
+        self.init_op = tf.global_variables_initializer()
+        # setup the initialisation operator
+        self.writer = tf.summary.FileWriter('output')
+        tf.summary.scalar('accuracy', self.accuracy)
 
-# define an accuracy assessment operation
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    def next_batch(self, batch_size, X, y):
+        indices = np.random.choice(X.shape[0], batch_size, replace=False)
+        return X[:, indices], y[:, indices]
 
-# setup the initialisation operator
-writer = tf.summary.FileWriter('output')
-tf.summary.scalar('accuracy', accuracy)
+    def train(self, X, y, testX, testy, epochs, batch_size):
+        merged = tf.summary.merge_all()
+        with tf.Session() as sess:
+            # initialise the variables
+            sess.run(self.init_op)
+            total_batch = int(X.shape[0] / batch_size)
+            for epoch in range(epochs):
+                avg_cost = 0
+                for i in range(total_batch):
+                    batch_x, batch_y = self.next_batch(batch_size, X, y)
+                    _, c = sess.run([self.optimiser, self.cross_entropy],
+                                    feed_dict={self.x: batch_x, self.y: batch_y})
+                    avg_cost += c / total_batch
+                test_acc = sess.run(self.accuracy,
+                               feed_dict={self.x: testX, self.y: testy})
+                print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost), "test accuracy: {:.3f}".format(test_acc))
+                summary = sess.run(merged, feed_dict={self.x: testX, self.y: testy})
+                self.writer.add_summary(summary, epoch)
+            self.writer.add_graph(sess.graph)
+            print(sess.run(self.accuracy, feed_dict={self.x: testX, self.y: testy}))
+            print("\nTraining complete!")
+        print(sess.run(self.accuracy, feed_dict={self.x: testX, self.y: testy}))
 
-merged = tf.summary.merge_all()
-with tf.Session() as sess:
-    # initialise the variables
-    sess.run(init_op)
-    total_batch = int(len(mnist.train.labels) / batch_size)
-    for epoch in range(epochs):
-        avg_cost = 0
-        for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
-            _, c = sess.run([optimiser, cross_entropy],
-                            feed_dict={x: batch_x, y: batch_y})
-            avg_cost += c / total_batch
-        test_acc = sess.run(accuracy,
-                       feed_dict={x: mnist.test.images, y: mnist.test.labels})
-        print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost), "test accuracy: {:.3f}".format(test_acc))
-        summary = sess.run(merged, feed_dict={x: mnist.test.images, y: mnist.test.labels})
-        writer.add_summary(summary, epoch)
-    writer.add_graph(sess.graph)
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
-    print("\nTraining complete!")
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
+
+if __name__ == "__main__":
+    epochs = 5
+    batch_size = 500
+    nn = TensorFlowNeuralNetwork(28, 28)
+    nn.train(5, 10)
+
